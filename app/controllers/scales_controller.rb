02a7c13f73ca21a832736c163e59104c8bfef39e
haxlerose <chestertonpa@gmail.com>
2018-02-06 15:51:03 -0500

    Refactoring
    Refactored code to combine some if/else commands to shorten the code. Also made the scale names capitalized for all of the scales.

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -6,112 +6,62 @@ class ScalesController < ApplicationController
   def pitches
     @key = params[:key].capitalize
     @scale = params[:scale]
+    @i = 1
 
     load_keys
     (find_pattern) if @scale != "chromatic"
     (find_accidentals) if @scale != "chromatic"
 
-    @sharp_key ? load_sharps : load_flats
-
-    @i = 1
-
-    # returns chromatic array for keys with sharps
-    if @scale == "chromatic" && @chromatic_sharp_keys.include?(@key)
+    if @chromatic_sharp_keys.include?(@key) || @sharp_key
       load_sharps
-      while @i < 12 do
-        if @key_pos_sharp + @i < 12
-          @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i])
-          @i += 1
-        else
-          @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i - 12])
-          @i += 1
-        end
-      end
-      #render @scale_sharp
-    end
-
-    # returns chromatic array for keys with flats
-    if @scale == "chromatic" && @chromatic_flat_keys.include?(@key)
+    else
       load_flats
+    end
+
+    if @scale == "chromatic"
       while @i < 12 do
-        if @key_pos_flat + @i < 12
-          @scale_flat.push(@all_flat_notes[@key_pos_flat + @i])
+        if @key_pos + @i < 12
+          @scale_build.push(@all_notes[@key_pos + @i])
           @i += 1
         else
-          @scale_flat.push(@all_flat_notes[@key_pos_flat + @i - 12])
+          @scale_build.push(@all_notes[@key_pos + @i - 12])
           @i += 1
         end
       end
-      #render @scale_flat
     end
 
-    # returns array for non-chromatic scales with sharps
-    if @scale != "chromatic" && @sharp_key
+    if @scale != "chromatic"
       while @i < @pattern_array.length do
-        if @key_pos_sharp + @i < @pattern_array.length
+        if @key_pos + @i < @pattern_array.length
           if @pattern_array[@i - 1] == "m"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i])
+            @scale_build.push(@all_notes[@key_pos + @i])
             @i += 1
           elsif @pattern_array[@i - 1] == "M"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i + 1])
-            @key_pos_sharp +=1
+            @scale_build.push(@all_notes[@key_pos + @i + 1])
+            @key_pos +=1
             @i += 1
           elsif @pattern_array[@i - 1] == "A"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i + 2])
-            @key_pos_sharp +=2
+            @scale_build.push(@all_notes[@key_pos + @i + 2])
+            @key_pos +=2
             @i += 1
           end
         else
           if @pattern_array[@i - 1] == "m"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i - 12])
+            @scale_build.push(@all_notes[@key_pos + @i - 12])
             @i += 1
           elsif @pattern_array[@i - 1] == "M"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i + 1 - 12])
-            @key_pos_sharp += 1
+            @scale_build.push(@all_notes[@key_pos + @i + 1 - 12])
+            @key_pos += 1
             @i += 1
           elsif @pattern_array[@i - 1] == "A"
-            @scale_sharp.push(@all_sharp_notes[@key_pos_sharp + @i + 2 - 12])
-            @key_pos_sharp += 2
+            @scale_build.push(@all_notes[@key_pos + @i + 2 - 12])
+            @key_pos += 2
             @i += 1
           end
         end
       end
-      #render @scale_sharp
     end
-
-    # returns array for non-chromatic scales with flats
-    if @scale != "chromatic" && @sharp_key != true
-      while @i < @pattern_array.length do
-        if @key_pos_flat + @i < @pattern_array.length
-          if @pattern_array[@i - 1] == "m"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i])
-            @i += 1
-          elsif @pattern_array[@i - 1] == "M"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i + 1])
-            @key_pos_flat += 1
-            @i += 1
-          elsif @pattern_array[@i - 1] == "A"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i + 2])
-            @key_pos_flat += 2
-            @i += 1
-          end
-        else
-          if @pattern_array[@i - 1] == "m"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i - 12])
-            @i += 1
-          elsif @pattern_array[@i - 1] == "M"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i + 1 - 12])
-            @key_pos_flat += 1
-            @i += 1
-          elsif @pattern_array[@i - 1] == "A"
-            @scale_flat.push(@all_flat_notes[@key_pos_flat + @i + 2 - 12])
-            @key_pos_flat += 2
-            @i += 1
-          end
-        end
-      end
-      #render @scale_flat
-    end
+    capitalize_scale_names
     render "home"
   end
 
@@ -140,52 +90,66 @@ class ScalesController < ApplicationController
   end
 
   def find_accidentals
-  (@pattern_array = @pattern.scan /\w/) if @scale != "chromatic"
+  @pattern_array = @pattern.scan /\w/
 
-  # finds if the pattern has a major or minor third
-  if @pattern != nil
-    @third = 0
-    case @pattern_array[0]
-    when "m"
-      @third += 1
-    when "M"
-      @third += 2
-    when "A"
-      @third += 3
+    # finds if the pattern has a major or minor third
+    if @pattern
+      @third = 0
+      case @pattern_array[0]
+      when "m"
+        @third += 1
+      when "M"
+        @third += 2
+      when "A"
+        @third += 3
+      end
+
+      case @pattern_array[1]
+      when "m"
+        @third += 1
+      when "M"
+        @third += 2
+      when "A"
+        @third += 3
+      end
     end
 
-    case @pattern_array[1]
-    when "m"
-      @third += 1
-    when "M"
-      @third += 2
-    when "A"
-      @third += 3
+    # finds if the scale uses sharps or flats
+    if @third == 3 && @minor_flat_keys.include?(@key) && @scale != "octatonic"
+      @sharp_key = false
+    elsif @third == 4 && @chromatic_flat_keys.include?(@key) && @scale != "octatonic"
+      @sharp_key = false
+    elsif @scale == "octatonic" && @octatonic_flat_keys.include?(@key)
+      @sharp_key = false
+    else
+      @sharp_key = true
     end
   end
 
-  # finds if the scale uses sharps or flats
-  if @third == 3 && @minor_flat_keys.include?(@key) && @scale != "octatonic"
-    @sharp_key = false
-  elsif @third == 4 && @chromatic_flat_keys.include?(@key) && @scale != "octatonic"
-    @sharp_key = false
-  elsif @scale != "octatonic" && @octatonic_flat_keys.include?(@key)
-    @sharp_key = false
-  else
-    @sharp_key = true
+  def load_sharps
+    @all_notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    @key_pos = @all_notes.find_index(@key)
+    @scale_build = [@key]
+    (@sharp_key = true) if @sharp_key == nil
   end
-end
 
-def load_sharps
-  @all_sharp_notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-  @key_pos_sharp = @all_sharp_notes.find_index(@key)
-  @scale_sharp = [@key]
-end
+  def load_flats
+    @all_notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+    @key_pos = @all_notes.find_index(@key)
+    @scale_build = [@key]
+    (@sharp_key = false) if @sharp_key == nil
+  end
 
-def load_flats
-  @all_flat_notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-  @key_pos_flat = @all_flat_notes.find_index(@key)
-  @scale_flat = [@key]
-end
-
+  def capitalize_scale_names
+    case @scale
+    when "harmonic_minor"
+      @scale = "Harmonic Minor"
+    when "major_pentatonic"
+      @scale = "Major Pentatonic"
+    when "minor_pentatonic"
+      @scale = "Minor Pentatonic"
+    else
+      @scale = @scale.capitalize
+    end
+  end
 end
